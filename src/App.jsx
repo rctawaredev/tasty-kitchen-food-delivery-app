ee977861cc52e630d09417f8b6dd172a774cbbf9
Rohan Taware <rohantaware.work@gmail.com>
2026-02-23 10:36:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Login from './components/Login.jsx'
+
+function App() {
+  return (
+      <div>
+       <Login/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,118 @@
+import { useState } from "react";
+import { IoEyeOutline, IoEyeOffSharp } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const [showErrorMsg, setShowErrorMsg] = useState(false);
+
+  const navigate = useNavigate();
+
+  const onSuccessLogin = (jwtToken) => {
+    Cookies.set("jwt_token", jwtToken, { expires: 30 });
+    navigate("/", { replace: true });
+  };
+
+  const onFailureLogin = (msg) => {
+    setErrorMsg(msg);
+    setShowErrorMsg(true);
+  };
+
+  const onSignIn = async (event) => {
+    event.preventDefault();
+    const userDetails = { username, password };
+
+    const response = await fetch("https://apis.ccbp.in/login", {
+      method: "POST",
+      body: JSON.stringify(userDetails),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      onSuccessLogin(data.jwt_token);
+    } else {
+      onFailureLogin(data.error_msg);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row bg-white relative">
+      <img
+        src="https://res.cloudinary.com/distnojxb/image/upload/v1771817606/login_t4kb51.png"
+        alt="login"
+        className="absolute top-[-38px] w-[387px] h-[387px] ml-[56px] md:hidden self-end object-cover rounded-l-[208px] rounded-br-[180px] pb-[50px]"
+      />
+
+      {/* 🟠 LEFT FORM */}
+      <div className="flex flex-col justify-center items-center w-full md:w-1/2 pt-[350px] md:pt-0 px-6">
+        <form
+          onSubmit={onSignIn}
+          className="bg-white flex flex-col p-6 md:p-10 shadow-md md:shadow-xl rounded-lg"
+        >
+          <h1 className="text-3xl font-medium text-[#0F172A] text-center pb-6">
+            Login
+          </h1>
+
+          <label className="text-xs font-[700] leading-[16px] tracking-[1%] text-[#475569] pb-2">USERNAME</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+            className="p-2 w-full md:w-80 rounded-sm outline-none bg-[#E2E8F0] text-[#475569] placeholder:text-sm mb-4"
+          />
+
+          <label className="text-xs font-[700] leading-[16px] tracking-[1%] text-[#475569] pb-2">PASSWORD</label>
+
+          <div className="relative w-74 md:w-80">
+            <input
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              className="p-2 w-full rounded-sm outline-none bg-[#E2E8F0] text-[#475569] pr-10"
+            />
+
+            <span
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
+            >
+              {showPassword ? (
+                <IoEyeOffSharp className="text-[#475569]" />
+              ) : (
+                <IoEyeOutline className="text-[#475569]" />
+              )}
+            </span>
+          </div>
+
+          {showErrorMsg && (
+            <p className="text-red-500 text-sm pt-2">{errorMsg}</p>
+          )}
+
+          <button
+            type="submit"
+            className="bg-[#F7931E] w-full md:w-80 p-2 rounded-md mt-6 text-white"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+
+
+      <div className="hidden md:block w-1/2">
+        <img
+          src="https://res.cloudinary.com/distnojxb/image/upload/v1771817606/login_t4kb51.png"
+          alt="login"
+          className="w-full h-screen object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
